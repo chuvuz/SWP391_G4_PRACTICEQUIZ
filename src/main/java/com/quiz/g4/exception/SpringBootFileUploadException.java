b79a19a0a23407521b182a04fbf1b2647f28c592
vux <vucqhe150798@fpt.edu.vn>
2024-09-06 16:34:39 +0700

    Delete SpringBootFileUploadException.java

--- src/main/java/com/quiz/g4/exception/SpringBootFileUploadException.java
+++
@@ -1,9 +0,0 @@
-package com.quiz.g4.exception;
-
-public class SpringBootFileUploadException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    public SpringBootFileUploadException(String message) {
-        super(message);
-    }
-}
