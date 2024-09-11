5d2c87eddc4ee6e219a26bf608d651e5c4ed307f
vux <vucqhe150798@fpt.edu.vn>
2024-09-11 18:31:03 +0700

    update header and config

--- src/main/java/com/quiz/g4/entity/User.java
+++ src/main/java/com/quiz/g4/entity/User.java
@@ -36,7 +36,7 @@ public class User implements UserDetails {
 	@Column(name = "full_name", nullable = false)
 	private String fullName;
 
-	@Column(name = "description", nullable = false)
+	@Column(name = "description")
 	private String description;
 
 	@Column(name = "password", nullable = false)
