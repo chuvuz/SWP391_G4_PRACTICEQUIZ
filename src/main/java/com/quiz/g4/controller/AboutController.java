e319aaaa8c9862daff7b57d23ea255d37592ff6a
dat092k3 <datnthe171830@fpt.edu.vn>
2024-09-15 13:58:51 +0700

    update about

--- src/main/java/com/quiz/g4/controller/AboutController.java
+++ src/main/java/com/quiz/g4/controller/AboutController.java
@@ -1,11 +1,6 @@
-package com.quiz.g4.controller.authoController;
+package com.quiz.g4.controller;
 
-import com.quiz.g4.entity.Blog;
-import com.quiz.g4.entity.Quiz;
-import com.quiz.g4.entity.Subject;
 import com.quiz.g4.entity.User;
-import com.quiz.g4.service.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -14,53 +9,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.Objects;
+import com.quiz.g4.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
-public class HomeController {
-
+public class AboutController {
     @Autowired
     private UserService userService;
 
-    @Autowired
-    private SubjectService subjectService;
-
-    @Autowired
-    private QuizService quizService;
-
-    @Autowired
-    private BlogService blogService;
-
-    @GetMapping({"/", "/home"})
-    public String home(Model model) {
+    @GetMapping("/about")
+    public String aboutUs(Model model){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (Objects.nonNull(authentication) && authentication.isAuthenticated() && !authentication.getName().equals("anonymousUser")) {
             String email = authentication.getName();
             User user = userService.findByEmail(email);
             model.addAttribute("user", user);
         }
-
-        // Tạo một danh sách subject từ service
-        List<Subject> subjects = subjectService.getAllSubjects();
-
-        // Đưa danh sách subject vào model
-        model.addAttribute("subjects", subjects);
-
-
-        // Lấy danh sách quiz
-        List<Quiz> quizzes = quizService.getAllQuizzes();
-        model.addAttribute("quizzes", quizzes);
-
         // Lấy danh sách các user có role_id = 3 (ROLE_EXPERT)
         List<User> experts = userService.findByRoleId(3);
         // Đưa danh sách này vào model
         model.addAttribute("experts", experts);
-
-        List<Blog> blogs = blogService.findAllBlogs();
-        model.addAttribute("blogs", blogs);
-        return "home";
+        return "about";
     }
-
-
-
-
 }
