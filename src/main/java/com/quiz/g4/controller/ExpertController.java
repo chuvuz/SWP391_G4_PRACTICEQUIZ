cb5df670cf555038789373062e19c9c559c25bbe
dat092k3 <datnthe171830@fpt.edu.vn>
2024-09-18 12:44:14 +0700

    update Expert Detail

--- src/main/java/com/quiz/g4/controller/ExpertController.java
+++ src/main/java/com/quiz/g4/controller/ExpertController.java
@@ -1,6 +1,10 @@
 package com.quiz.g4.controller;
 
+import com.quiz.g4.entity.Quiz;
+import com.quiz.g4.entity.Subject;
 import com.quiz.g4.entity.User;
+import com.quiz.g4.service.QuizService;
+import com.quiz.g4.service.SubjectService;
 import com.quiz.g4.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,11 +17,18 @@ import java.util.List;
 
 @Controller
 public class ExpertController {
+
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private QuizService quizService;
+
+    @Autowired
+    private SubjectService subjectService;
+
     @GetMapping("/expert")
-    public String getAllExpert(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+    public String getExpertlist(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
                                @RequestParam(value = "size", defaultValue = "8") int size){
         page = Math.max(page, 0);
         // Lấy danh sách các user có role_id = 3 (ROLE_EXPERT)
@@ -26,5 +37,22 @@ public class ExpertController {
         model.addAttribute("expertPage", expertPage);
         return "experts";
     }
+
+    @GetMapping("/expert_detail")
+    public String getExpertDetails(Model model,
+                                   @RequestParam(value = "page", defaultValue = "0") int page,
+                                   @RequestParam(value = "size", defaultValue = "10") int size){
+        // Lấy danh sách các subject và expert để hiển thị
+        List<Subject> subjects = subjectService.getAllSubjects();
+        List<User> experts = userService.findByRoleId(3); // Expert có role_id = 3
+        model.addAttribute("subjects", subjects);
+        model.addAttribute("experts", experts);
+
+        // Lấy danh sách quiz
+        Page<Quiz> quizPage = quizService.getAllQuizzes(page, size);
+        model.addAttribute("quizPage", quizPage);
+        return "expert_details";
+    }
+
 
 }
