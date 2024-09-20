ec7c25162156958d2d1049dc76f167907a8a06ad
huyhnhe173365 <huyhnhe173365@fpt.edu.vn>
2024-09-20 16:01:06 +0700

    update register function without validation

--- src/main/java/com/quiz/g4/repository/RoleRepository.java
+++ src/main/java/com/quiz/g4/repository/RoleRepository.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+
+
+    Role findRoleByRoleId(Integer roleId);
 }
