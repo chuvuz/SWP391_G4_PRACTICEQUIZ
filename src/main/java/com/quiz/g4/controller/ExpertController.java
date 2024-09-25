1cfac906c0f55d627e84a33572805f81d01f3383
dat092k3 <datnthe171830@fpt.edu.vn>
2024-09-25 13:40:24 +0700

    Update ExpertController.java

--- src/main/java/com/quiz/g4/controller/ExpertController.java
+++ src/main/java/com/quiz/g4/controller/ExpertController.java
@@ -4,7 +4,6 @@ import com.quiz.g4.entity.Quiz;
 import com.quiz.g4.entity.Subject;
 import com.quiz.g4.entity.User;
 import com.quiz.g4.service.QuizService;
-import com.quiz.g4.service.SubjectService;
 import com.quiz.g4.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -26,8 +25,6 @@ public class ExpertController {
     @Autowired
     private QuizService quizService;
 
-    @Autowired
-    private SubjectService subjectService;
 
     @GetMapping("/expert")
     public String getExpertlist(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
