8add6c4361d7e0442c5fa8097f763d3fc8be92f4
dat092k3 <datnthe171830@fpt.edu.vn>
2024-10-16 04:54:32 +0700

    update question bank

--- src/main/java/com/quiz/g4/repository/QuestionBankRepository.java
+++ src/main/java/com/quiz/g4/repository/QuestionBankRepository.java
@@ -1,18 +1,9 @@
 package com.quiz.g4.repository;
 
 import com.quiz.g4.entity.QuestionBank;
-<<<<<<< Updated upstream
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionBankRepository extends JpaRepository<QuestionBank, Integer> {
-=======
-import com.quiz.g4.entity.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface QuestionBankRepository extends JpaRepository<QuestionBank,Integer> {
->>>>>>> Stashed changes
 }
