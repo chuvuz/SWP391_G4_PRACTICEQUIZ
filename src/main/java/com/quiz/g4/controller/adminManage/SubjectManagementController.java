58040a0008d009ad8097d004e5f5340cd76b4a8f
huyhnhe173365 <huyhnhe173365@fpt.edu.vn>
2024-10-21 09:38:39 +0700

    update manage-subject

--- src/main/java/com/quiz/g4/controller/adminManage/SubjectManagementController.java
+++ src/main/java/com/quiz/g4/controller/adminManage/SubjectManagementController.java
@@ -1,7 +1,9 @@
 package com.quiz.g4.controller.adminManage;
 
 import com.quiz.g4.entity.Subject;
+import com.quiz.g4.entity.User;
 import com.quiz.g4.repository.SubjectRepository;
+import com.quiz.g4.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
+import java.security.Principal;
 import java.util.Optional;
 
 @Controller
@@ -22,12 +25,26 @@ public class SubjectManagementController {
     @Autowired
     private SubjectService subjectService;
     @Autowired
+    private UserService userService;
+
+    @Autowired
     private SubjectRepository subjectRepository;
     @GetMapping("/manage-subject")
-    public String manageSubject(Model model) {
+    public String manageSubject(Model model, Principal principal) {
+        // Lấy thông tin người dùng đã đăng nhập thông qua principal
+        if (principal != null) {
+            // Sử dụng email từ principal để tìm kiếm người dùng
+            User user = userService.findByEmail(principal.getName());
+            model.addAttribute("user", user); // Truyền thông tin người dùng vào model
+        }
+
+        // Truyền danh sách các môn học vào model
         model.addAttribute("subjects", subjectService.getAllSubjects());
+
+        // Trả về trang admin/manage-subject
         return "admin/manage-subject";
     }
+
 
 
     @PostMapping("/create-subject")
@@ -38,11 +55,21 @@ public class SubjectManagementController {
     }
 
     @GetMapping("/edit-subject/{subjectId}")
-    public String editSubjectForm(Model model, @PathVariable Integer subjectId) {
+    public String editSubjectForm(Model model, @PathVariable Integer subjectId, Principal principal) {
+        // Kiểm tra xem người dùng có đăng nhập hay không
+        if (principal != null) {
+            // Lấy thông tin người dùng đã đăng nhập
+            User user = userService.findByEmail(principal.getName());
+            model.addAttribute("user", user);
+        }
+
+        // Lấy thông tin subject theo subjectId
         Subject subject = subjectService.findById(subjectId);
-        model.addAttribute("subject",subject);
+        model.addAttribute("subject", subject);
+
         return "admin/edit-subject";
     }
+
 
     @PostMapping("/edit-subject")
     public String updateSubject(
