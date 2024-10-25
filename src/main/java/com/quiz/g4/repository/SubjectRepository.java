7b81a7dad9892002a6792063c6737f24ccd74089
vux <vucqhe150798@fpt.edu.vn>
2024-10-25 17:11:43 +0700

    Update SubjectRepository.java

--- src/main/java/com/quiz/g4/repository/SubjectRepository.java
+++ src/main/java/com/quiz/g4/repository/SubjectRepository.java
@@ -20,7 +20,7 @@ public interface SubjectRepository extends JpaRepository<Subject,Integer> {
     @Query("SELECT s FROM Subject s WHERE s.isActive = true")
     Page<Subject> findAllActiveSubject(Pageable pageable);
 
-    @Query("SELECT s FROM Subject s WHERE s.subjectName LIKE %:subjectName%")
+    @Query("SELECT s FROM Subject s WHERE s.subjectName LIKE %:subjectName% AND s.isActive = true")
     Page<Subject> searchSubject(@Param("subjectName") String subjectName, Pageable pageable);
 
 
